911ae2ced68c2fdc1d2b016df3b08630636e1257
John <johnsmilga@gmail.com>
2020-08-05 09:36:24 -0700

    initial map commit

---
+++ 1-map/app.js
@@ -0,0 +1,21 @@
+const people = [
+  {
+    name: 'bob',
+    age: 20,
+    position: 'developer',
+  },
+  {
+    name: 'anna',
+    age: 25,
+    position: 'designer',
+  },
+  {
+    name: 'susy',
+    age: 30,
+    position: 'the boss',
+  },
+];
+
+// returns a new array
+// does not change the size of original array (unlike filter)
+// uses values from original array when making new one
