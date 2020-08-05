59971326db70d56babcb1e167b3c08a518c9d1c8
john-smilga <johnsmilga@gmail.com>
2020-08-05 10:19:16 -0700

    map complete

--- 1-map/app.js
+++ 1-map/app.js
@@ -1,3 +1,4 @@
+// MAP Method
 const people = [
   {
     name: 'bob',
@@ -14,8 +15,31 @@ const people = [
     age: 30,
     position: 'the boss',
   },
+  {
+    name: 'john',
+    age: 26,
+    position: 'intern',
+  },
 ];
-
+// Quokka.js - Extension
 // returns a new array
 // does not change the size of original array (unlike filter)
 // uses values from original array when making new one
+
+const getAges = (person) => person.age * 2;
+
+const ages = people.map(getAges);
+console.log(ages);
+
+const newPeople = people.map((item) => {
+  return {
+    firsName: item.name.toUpperCase(),
+    oldAge: item.age + 20,
+  };
+});
+console.log(newPeople);
+
+const names = people.map((person) => `<h2>${person.name}</h2>`);
+const result = document.querySelector('#result');
+
+result.innerHTML = names.join('');
