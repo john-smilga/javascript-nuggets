9c1465ee6dea3dd3ff56361eede1f4fde395e366
John <johnsmilga@gmail.com>
2020-08-15 07:54:05 -0700

    2-unique values added

---
+++ 2-unique-values/app.js
@@ -0,0 +1,38 @@
+// Unique Values
+const menu = [
+  {
+    name: 'pancakes',
+    category: 'breakfast',
+  },
+  {
+    name: 'burger',
+    category: 'lunch',
+  },
+  {
+    name: 'steak',
+    category: 'dinner',
+  },
+  {
+    name: 'bacon',
+    category: 'breakfast',
+  },
+  {
+    name: 'eggs',
+    category: 'breakfast',
+  },
+  {
+    name: 'pasta',
+    category: 'dinner',
+  },
+];
+// const categories = ['all', ...new Set(menu.map((item) => item.category))];
+// map - get all instances
+// new Set - narrow down
+// ['all',...] - turn it back to array
+
+const result = document.querySelector('.result');
+result.innerHTML = categories
+  .map((category) => {
+    return `<button>${category}</button>`;
+  })
+  .join('');
