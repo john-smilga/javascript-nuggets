a343c5ab8965cbf18aae11da9723adb3593d5935
john-smilga <johnsmilga@gmail.com>
2021-05-17 11:34:03 -0700

    cb-hell complete, complete

--- 14-cb-hell/app.js
+++ 14-cb-hell/app.js
@@ -7,7 +7,6 @@
 const first = document.querySelector('.first')
 const second = document.querySelector('.second')
 const third = document.querySelector('.third')
-
 const btn = document.querySelector('.btn')
 
 btn.addEventListener('click', () => {
