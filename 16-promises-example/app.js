f4a912bc638b4fab294077c66aeef0347a1a172f
john-smilga <johnsmilga@gmail.com>
2021-06-04 09:30:39 -0700

    16-promises example

---
+++ 16-promises-example/app.js
@@ -0,0 +1,29 @@
+//  Javascript Nuggets - Promises Example
+// .firt - after 1s first red;
+// .second - after 3s second blue; 4s
+// .third - after 2s third green; 6s
+// IN SEQUENCE !!!!
+
+const btn = document.querySelector('.btn')
+
+btn.addEventListener('click', () => {
+  addColor(1000, '.first', 'red')
+    .then(() => addColor(3000, '.second', 'blue'))
+    .then(() => addColor(2000, '.third', 'green'))
+    .catch((err) => console.log(err))
+})
+
+function addColor(time, selector, color) {
+  const element = document.querySelector(selector)
+  return new Promise((resolve, reject) => {
+    if (element) {
+      setTimeout(() => {
+        element.style.color = color
+
+        resolve()
+      }, time)
+    } else {
+      reject(`There is no such element : "${selector}"`)
+    }
+  })
+}
