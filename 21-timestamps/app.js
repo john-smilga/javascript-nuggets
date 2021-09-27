e1cb3dea4358f4396e26987c9d63cd0cbc281679
john-smilga <johnsmilga@gmail.com>
2021-09-26 20:58:43 -0700

    21-timestamps

---
+++ 21-timestamps/app.js
@@ -0,0 +1,51 @@
+//  Javascript Nuggets
+//  Timestamps
+console.log(new Date());
+
+// Unix Time
+// January 1, 1970
+// 1s = 1000ms
+
+// Date.now()
+console.log(Date.now());
+// new Date().getTime()
+console.log(new Date().getTime());
+// new Date().valueOf()
+console.log(new Date().valueOf());
+
+setTimeout(() => {
+  console.log(Date.now());
+}, 1000);
+
+// create id's in learning apps
+let people = [];
+people = [...people, { id: Date.now(), name: 'peter' }];
+
+// setTimeout(() => {
+//   people = [...people, { id: Date.now(), name: 'john' }];
+//   console.log(people);
+// }, 1000);
+
+// create/get dates
+console.log(new Date(1132713260838));
+const now = Date.now();
+const futureDate = new Date(now + 60000);
+console.log(futureDate);
+console.log(new Date());
+
+// difference between dates
+const firstDate = new Date();
+const secondDate = new Date(2021, 8, 28);
+
+const firstValue = firstDate.getTime();
+const secondValue = secondDate.getTime();
+
+console.log(firstValue);
+console.log(secondValue);
+const timeDifference = secondValue - firstValue;
+
+const minutes = timeDifference / (1000 * 60);
+console.log(minutes);
+
+const hours = timeDifference / (1000 * 60 * 60);
+console.log(hours);
